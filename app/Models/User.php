b89845e07b247888346740fab3517de8c3d6c92b
Henri Okayama <henri.okayama@gmail.com>
2025-06-18 21:52:45 -0300

    feat: users list page

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -45,6 +46,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'settings' => 'array',
+            'is_admin' => 'boolean',
         ];
     }
 
