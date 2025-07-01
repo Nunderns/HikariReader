e532f927986890cce661fd7903e37155fd36f488
Henri Okayama <henri.okayama@gmail.com>
2025-06-30 23:49:55 -0300

    feat: terms and policies page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -147,6 +147,12 @@
                             </svg>
                             Sobre
                         </a>
+                        <a href="{{ route('terms') }}" class="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-50">
+                            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+                            </svg>
+                            Terms & Policies
+                        </a>
                     </div>
                 </nav>
             </div>
